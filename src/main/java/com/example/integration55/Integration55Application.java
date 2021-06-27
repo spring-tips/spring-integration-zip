f78cd1dd0e31dee6935164b405579fbe6bd8de3a
Josh Long <josh@joshlong.com>
2021-06-26 17:14:14 -0700

    SIZ

---
+++ src/main/java/com/example/integration55/Integration55Application.java
@@ -0,0 +1,65 @@
+package com.example.integration55;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.file.FileHeaders;
+import org.springframework.integration.file.dsl.Files;
+import org.springframework.integration.zip.splitter.UnZipResultSplitter;
+import org.springframework.integration.zip.transformer.UnZipTransformer;
+import org.springframework.integration.zip.transformer.ZipTransformer;
+
+import java.io.File;
+
+@SpringBootApplication
+public class Integration55Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Integration55Application.class, args);
+	}
+
+	@Bean
+	UnZipTransformer read() {
+		return new UnZipTransformer();
+	}
+
+	@Bean
+	UnZipResultSplitter unZipResultSplitter() {
+		return new UnZipResultSplitter();
+	}
+
+	@Bean
+	ZipTransformer write() {
+		return new ZipTransformer();
+	}
+
+	@Bean
+	IntegrationFlow files(
+		UnZipTransformer read,
+		UnZipResultSplitter unZipResultSplitter,
+		ZipTransformer write,
+		@Value("file:///${user.home}/Desktop/inbound") File in,
+		@Value("file:///${user.home}/Desktop/outbound") File out) {
+		var inbound = Files
+			.inboundAdapter(in)
+			.autoCreateDirectory(true);
+		return IntegrationFlows
+			.from(inbound, pm -> pm.poller(pc -> pc.fixedRate(1000)))
+			.transform(read)
+			.split(unZipResultSplitter)
+			.transform(write)
+			.handle(Files
+				.outboundAdapter(out)
+				.autoCreateDirectory(true)
+				.fileNameGenerator(in1 -> in1.getHeaders().get(FileHeaders.FILENAME) + ".zip")
+			)
+			.get();
+	}
+
+
+}
+
+
---
+++ src/test/java/com/example/integration55/Integration55ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.integration55;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Integration55ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
